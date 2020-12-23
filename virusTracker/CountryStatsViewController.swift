ef015aa510d04bb86e0fe2864c27bc1bac4a2462
Nada Zeini <nada.elzeini@sjsu.edu>
2020-12-23 01:08:09 -0800

    Display date of last update

--- virusTracker/CountryStatsViewController.swift
+++ virusTracker/CountryStatsViewController.swift
@@ -9,7 +9,7 @@
 import UIKit
 
 class CountryStatsViewController: UIViewController {
-     var country = ""
+    var country = ""
     @IBOutlet weak var redView: UIView!
     @IBOutlet weak var orangeView: UIView!
     @IBOutlet weak var greenView: UIView!
@@ -19,75 +19,82 @@ class CountryStatsViewController: UIViewController {
     @IBOutlet weak var activeCases: UILabel!
     @IBOutlet weak var recoveredCases: UILabel!
     @IBOutlet weak var confirmedCases: UILabel!
+    @IBOutlet weak var latestDate: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         let textAttributes = [NSAttributedString.Key.foregroundColor:UIColor.red]
         navigationController?.navigationBar.titleTextAttributes = textAttributes
-              self.navigationItem.title = country
-              print("here \(SearchTableViewController.selectedCountry)")
-       
+        self.navigationItem.title = country
+        print("here \(SearchTableViewController.selectedCountry)")
         redView.layer.cornerRadius = 10
         orangeView.layer.cornerRadius = 10
         greenView.layer.cornerRadius = 10
         blackView.layer.cornerRadius = 10
-//        let countryStats: Stat = getStat(country: SearchTableViewController.selectedCountry)
+        //why doesnt this work? make reusable!
+        //        let countryStats: Stat = getStat(country: SearchTableViewController.selectedCountry)
         
         let url = URL(string: "https://api.covid19api.com/dayone/country/\(SearchTableViewController.selectedCountry)")
-        var countryStat = Stat(Country: country, Confirmed: 0, Active: 0, Recovered: 0, Deaths: 0, Date: "")
+        var countryStat = Stat(Country: country, Confirmed: 0, Active: 0, Recovered: 0, Deaths: 0, Date: "N/A")
         //        print(AppDelegate.stats.count)
-                guard let requestUrl = url else {
-                    confirmedCases.text = "N/A"
-                    recoveredCases.text = "N/A"
-                    deathsCases.text = "N/A"
-                    activeCases.text = "N/A"
-                    return  }
-                    // Create URL Request
-                    var request = URLRequest(url: requestUrl)
-                    // Specify HTTP Method to use
-                    request.httpMethod = "GET"
-                    // Send HTTP Request
-                    let task = URLSession.shared.dataTask(with: request) { (data, response, error) in
-                        // Check if Error took place
-                        if let error = error {
-                            print("Error took place \(error)")
-                            return
-                        }
-                        // Read HTTP Response Status code
+        guard let requestUrl = url else {
+            confirmedCases.text = "N/A"
+            recoveredCases.text = "N/A"
+            deathsCases.text = "N/A"
+            activeCases.text = "N/A"
+            latestDate.text = "N/A"
+            return  }
+        // Create URL Request
+        var request = URLRequest(url: requestUrl)
+        // Specify HTTP Method to use
+        request.httpMethod = "GET"
+        // Send HTTP Request
+        let task = URLSession.shared.dataTask(with: request) { (data, response, error) in
+            // Check if Error took place
+            if let error = error {
+                print("Error took place \(error)")
+                return
+            }
+            // Read HTTP Response Status code
             //            if let response = response as? HTTPURLResponse {
             //                print("Response HTTP Status code: \(response.statusCode)")
             //            }
-                        let decoder = JSONDecoder()
-                        // Convert HTTP Response Data to a simple String
-                        if let data = data, let dataString = String(data: data, encoding: .utf8) {
+            let decoder = JSONDecoder()
+            // Convert HTTP Response Data to a simple String
+            if let data = data, let dataString = String(data: data, encoding: .utf8) {
                 //                print("Response data string:\n \(dataString)")
-                            do{
-                                let stat = try decoder.decode([Stat].self, from: dataString.data(using: .utf8)!) //array
-                                print(stat)
-                                if stat.count == 0 {
-                                    return
-                                }
-                                countryStat.Active = stat[stat.count-1].Active
-                                countryStat.Recovered = stat[stat.count-1].Recovered
-                                countryStat.Confirmed = stat[stat.count-1].Confirmed
-                                countryStat.Deaths = stat[stat.count-1].Deaths
-                                countryStat.Date = stat[stat.count-1].Date
-                                self.navigationItem.title = countryStat.Country.replacingOccurrences(of: "-", with: " ")
-        //                        AppDelegate.stats.append(stat[stat.count-1])
-                                //added each stat to stats array of stat objects
-                                DispatchQueue.main.async {
-                                    self.confirmedCases.text = String(countryStat.Confirmed)
-                                    self.recoveredCases.text = String(countryStat.Recovered)
-                                    self.deathsCases.text = String(countryStat.Deaths)
-                                    self.activeCases.text = String(countryStat.Active)
-                                }
-                                
-                            } catch {
-                                print(error)
-                            }
-                            
-                        }
+                do{
+                    let stat = try decoder.decode([Stat].self, from: dataString.data(using: .utf8)!) //array
+                    print(stat)
+                    if stat.count == 0 {
+                        return
                     }
-        
+                    countryStat.Active = stat[stat.count-1].Active
+                    countryStat.Recovered = stat[stat.count-1].Recovered
+                    countryStat.Confirmed = stat[stat.count-1].Confirmed
+                    countryStat.Deaths = stat[stat.count-1].Deaths
+                    //format date string
+                    let dateStr = stat[stat.count-1].Date
+                    let dateFormatter = DateFormatter()
+                    dateFormatter.locale = Locale(identifier: "en_US_POSTFIX")
+                    dateFormatter.dateFormat = "yyyy-MM-dd'T'HH:mm:ssZ"
+                    let date = dateFormatter.date(from: dateStr)!
+                    dateFormatter.dateFormat = "MM - dd - yyyy"
+                    let dateDisplayed = dateFormatter.string(from: date)
+                    countryStat.Date = dateDisplayed
+                    self.navigationItem.title = countryStat.Country.replacingOccurrences(of: "-", with: " ")
+                    DispatchQueue.main.async {
+                        self.confirmedCases.text = String(countryStat.Confirmed)
+                        self.recoveredCases.text = String(countryStat.Recovered)
+                        self.deathsCases.text = String(countryStat.Deaths)
+                        self.activeCases.text = String(countryStat.Active)
+                        self.latestDate.text = String(countryStat.Date)
+                    }
+                } catch {
+                    print(error)
+                }
+            }
+        }
         task.resume()
     }
 }
